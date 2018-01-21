da256723eeef100f08b62ad0e12c3179fcd8b3df
lanxin95 <lexyjiang@gmail.com>
2018-01-21 13:57:14 -0500

    Initial commit

---
+++ HW1P1.py
@@ -0,0 +1,15 @@
+#Problem 1: Object Types
+#1
+y1=int(17)
+print(y1,type(y1))
+y2=float(17)
+print(y2,type(y2))
+y3=str(17)
+print(y3,type(y3))
+y4=(17>16)
+print(y4,type(y4))
+#2
+text = "The value of y1, y2, or y3 is %d, %f, or %s." % (y1,y2,y3)
+print(text)
+text = "The value of y3 is %s." % (y3)
+print(text)
