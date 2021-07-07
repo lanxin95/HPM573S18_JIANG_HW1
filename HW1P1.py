2f0e212f8c247b412cf693f4e85e583c2134bf2d
lanxin95 <lexyjiang@gmail.com>
2021-07-07 11:30:20 -0700

    Update HW1P1.py
    test

--- HW1P1.py
+++ HW1P1.py
@@ -1,10 +1,10 @@
 #Problem 1: Object Types. Consider the number 17. Construct multiple variables (y1, y2, y3, and y4) in Python that represent the number 17 in the various forms of objects (integer, float, string, and Boolean, respectively). Hint: For creation of the Boolean, set a value for 17 to be compared against another number.
 #1. Print the value of y1, y2, y3, and y4 and their types (Hint: you can use function type() to get a type of an object or variable).
-y1=int(17)
+y1=int(18)
 print(y1,type(y1))
-y2=float(17)
+y2=float(18)
 print(y2,type(y2))
-y3=str(17)
+y3=str(18)
 print(y3,type(y3))
 y4=(17>16)
 print(y4,type(y4))
